3c76d89ada9f13a7bb5acbe16d3092e0b16ffb74
yamacacan <114859112+yamacacan@users.noreply.github.com>
2022-11-20 01:01:08 +0300

    Update capitalize a string.py

--- capitalize a string.py
+++ capitalize a string.py
@@ -4,19 +4,19 @@ Created on Thu Nov 17 15:17:43 2022
 
 @author: ahmet
 """
-
 def capitalize_str(a):
     newworldlist=a.split(" ")
     capstr=''
     for x in newworldlist:
-        if x!=" ":
+        if x:
           if x[0].isnumeric():
               capstr=capstr+x+" "
           else:
             x=x.capitalize()   
             capstr=capstr+x+" "
         else:
-           capstr=capstr+x 
+           capstr=capstr+x +" "
     return capstr
 a=input("Please enter a string ")         
-print(capitalize_str(a))      
+print(capitalize_str(a)) 
+        
