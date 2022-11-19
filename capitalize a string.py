36f88028b3a53680ce7acd3f182d70f46a5bdc6f
yamacacan <114859112+yamacacan@users.noreply.github.com>
2022-11-20 00:49:11 +0300

    Add files via upload

---
+++ capitalize a string.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 15:17:43 2022
+
+@author: ahmet
+"""
+
+def capitalize_str(a):
+    newworldlist=a.split(" ")
+    capstr=''
+    for x in newworldlist:
+        if x[0].isnumeric():
+            capstr=capstr+x+" "
+        else:
+            x=x.capitalize()   
+            capstr=capstr+x+" "
+    return capstr
+        
+a=input("Please enter a string ")         
+print(capitalize_str(a))      
